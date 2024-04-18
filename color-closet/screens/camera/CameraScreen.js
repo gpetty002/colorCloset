dfca1d9abd371bd9fa64c14adb32e6663039de35
gpetty002 <56804160+gpetty002@users.noreply.github.com>
2024-04-18 01:46:00 -0700

    Hooked up Camera and added to requirements.md - Added detailed To-Do for Color Closet in requirements - Attached Expo Camera to CameraScreen.js which can now take pictures and displays them on camera screen

--- color-closet/screens/camera/CameraScreen.js
+++ color-closet/screens/camera/CameraScreen.js
@@ -1,14 +1,87 @@
 // CameraScreen.js
 
-import React from "react";
-import { Button, View, Text } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, View, Text, Image, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CameraScreen = ({ navigation }) => {
+  const [cameraRef, setCameraRef] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [photoUri, setPhotoUri] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync();
+      setPhotoUri(photo.uri);
+    }
+  };
+
+  if (hasPermission === null) {
+    return (
+      <View>
+        <Text>Requesting camera permission...</Text>
+      </View>
+    );
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View>
+        <Text>No access to camera</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Welcome to Camera Screen</Text>
+    <View style={{ flex: 1 }}>
+      <Camera
+        style={{ flex: 1 }}
+        ref={(ref) => setCameraRef(ref)}
+        type={Camera.Constants.Type.back}
+      >
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <View style={styles.buttonOutline}></View>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {photoUri && <Image source={{ uri: photoUri }} style={{ flex: 1 }} />}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+    position: "absolute",
+    left: 160,
+    bottom: 20,
+  },
+  button: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonOutline: {
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: "white",
+    width: 80,
+    height: 80,
+  },
+});
 
 export default CameraScreen;
--- requirements.md
+++ requirements.md
@@ -5,3 +5,21 @@
 - Front end: React Native
 - Back end: Express.js
 - Database: MongoDB
+
+### To-Do
+
+- [ ] Clean up UI
+  - [ ] Add designs, images, and color
+  - [ ] Find color scheme to follow for entire app
+  - [ ] Create a CSS file containing project color palette theme
+- [ ] Figure out how to lock Featured, Camera, and Closet once we login/signup
+- [ ] Ensure login/signup is correctly protected
+- [ ] Attach MongoDB
+  - [ ] Become familiar with the database and figure out organization we want to follow
+  - [ ] Begin thinking of what makes each user different (email, password, outfits, images taken)
+  - [ ] Store the images taken on camera in the database and in the correct corresponding slot for the user
+  - [ ] Figure out to query this data after pinging the backend to receive this data
+- [ ] Research more ML libraries to abstract color pigments from photos taken
+  - [ ] Query color pigments from photo and suggest palettes
+  - [ ] Research AI API that will suggest color palettes that blend with the pigments abstracted from the photo
+- [ ] Move camera functionality to its own component in component folder to ensure smooth access in case user wants to change a photo and we need to use the camera again
